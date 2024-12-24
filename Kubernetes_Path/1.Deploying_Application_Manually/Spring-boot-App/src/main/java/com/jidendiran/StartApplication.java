cfc46820b450b41559cecd21f885b892eec6a2a8
Yogendiran-SP <jyogendiran@gmail.com>
2024-12-24 16:39:39 +0530

    after all update

--- Kubernetes_Path/1.Deploying_Application_Manually/Spring-boot-App/src/main/java/com/jidendiran/StartApplication.java
+++ Kubernetes_Path/1.Deploying_Application_Manually/Spring-boot-App/src/main/java/com/jidendiran/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
+        model.addAttribute("title", "I have successfuly built a spring application using Maven");
         model.addAttribute("msg", "This application is Built by Jenkins Pipeline");
         return "index";
     }
