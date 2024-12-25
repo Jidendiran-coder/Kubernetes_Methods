b283c22b3d852c25f43899f0964746d65a1a772a
jidendiran-coder <joel.jade.networking@gmail.com>
2024-12-25 22:25:17 +0530

    modified

--- Kubernetes_Path/2.Deploying_Container_Application/spring-boot-app/src/main/java/com/jidendiran/StartApplication.java
+++ Kubernetes_Path/2.Deploying_Container_Application/spring-boot-app/src/main/java/com/jidendiran/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is Built by Jenkins Pipeline");
+        model.addAttribute("title", "We have successfully built a Docker sprint boot application using Maven");
+        model.addAttribute("msg", "I'm Jidendiran, Congratulation on your Application Progress");
         return "index";
     }
 
